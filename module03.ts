df420553889019d44ed539bbe922449335011035
Thu Dinh Van(IT) <TDiVan1@FUSHAN.fihnbb.com>
2023-01-13 14:16:22 +0700

    remove check function

--- module03.ts
+++ module03.ts
@@ -28,8 +28,3 @@ function calculateConventionalLoanPayment(loanTerms: conventialLoan): string {
     payment = loanTerms.principal * interest / (1 - (Math.pow(1 / (1 + interest), loanTerms.months)));
     return 'The conventional loan payment is ' + payment.toFixed(2);
 }
-
-
-
-let conventionalPayment = calculateConventionalLoanPayment({principal: 30000, interestRate: 5, months: 180});
-console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24" 
